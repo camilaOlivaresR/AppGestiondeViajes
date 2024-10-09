50fb3740c49a98e2b4074b8258c2ffb3a34126e7
cami oliva <lamica.olivares@gmail.com>
2024-10-09 13:28:52 -0300

    devise view

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,5 +20,26 @@ usuario =   User.create!(
     email: 'usuario@mail.com',
     password: 'Abc123#', 
     password_confirmation: 'Abc123#',
-    role: %w[admin].sample 
+    role: %w[normal_user].sample 
     )
+
+    # Generar 6 usuarios aleatorios
+6.times do
+    User.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: 'password', # Puedes cambiar la contraseña si lo prefieres
+      password_confirmation: 'password',
+      role: %w[normal_user].sample # Selecciona al azar entre 'conductor' y 'supervisor'
+    )
+  end
+
+  20.times do
+    Itinerary.create!(
+      name: Faker::Lorem.paragraph(sentence_count: 2),
+      description: Faker::Lorem.paragraph(sentence_count: 2),  # Genera una descripción aleatoria
+      star_date: Faker::Date.backward(days: 30),  # Genera una fecha de salida en los últimos 30 días
+      end_date: Faker::Date.forward(days: 10),  # Genera una fecha de llegada en los próximos 10 días
+      user_id: User.pluck(:id).sample  # Selecciona un usuario al azar
+    )
+  end
